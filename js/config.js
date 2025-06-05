8adc31db9faa780ae25eb6c865045fe34d2306c8
calhelp <bastelix@web.de>
2025-06-06 00:28:36 +0200

    Add optional check answer button and auto-check on next

--- js/config.js
+++ js/config.js
@@ -3,5 +3,6 @@ window.quizConfig = {
   header: 'Sommerfest 2024',
   subheader: 'Willkommen beim Veranstaltungsquiz',
   backgroundColor: '#f8f8f8',
-  buttonColor: '#1e87f0'
+  buttonColor: '#1e87f0',
+  CheckAnswerButton: 'yes'
 };
