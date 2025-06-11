ed896ea524adba4cdada24f393307973160888d1
calhelp <bastelix@web.de>
2025-06-11 23:26:27 +0200

    Add Impressum and Lizenz pages

---
+++ src/Controller/ImpressumController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
+
+class ImpressumController
+{
+    public function __invoke(Request $request, Response $response): Response
+    {
+        $view = Twig::fromRequest($request);
+        return $view->render($response, 'impressum.twig');
+    }
+}
---
+++ tests/Controller/LizenzControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Controller;
+
+use Tests\TestCase;
+
+class LizenzControllerTest extends TestCase
+{
+    public function testLizenzPage(): void
+    {
+        $app = $this->getAppInstance();
+        $request = $this->createRequest('GET', '/lizenz');
+        $response = $app->handle($request);
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+}
