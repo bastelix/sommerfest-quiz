321d384ff86843473363be68a20a809770649a52
calhelp <bastelix@web.de>
2025-06-11 21:14:17 +0200

    Integrate Twig templating

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,13 +6,13 @@ namespace App\Controller;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
 
 class AdminController
 {
     public function __invoke(Request $request, Response $response): Response
     {
-        $path = dirname(__DIR__, 2) . '/templates/admin.html';
-        $response->getBody()->write(file_get_contents($path));
-        return $response;
+        $view = Twig::fromRequest($request);
+        return $view->render($response, 'admin.twig');
     }
 }
