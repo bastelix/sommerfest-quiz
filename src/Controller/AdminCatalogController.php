911498d2c47e6f6ee2cad666aae1a68d979c57c0
calhelp <bastelix@web.de>
2025-06-12 23:36:00 +0200

    Add admin base layout and catalog page

---
+++ src/Controller/AdminCatalogController.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
+use App\Service\CatalogService;
+
+class AdminCatalogController
+{
+    private CatalogService $service;
+
+    public function __construct(CatalogService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function __invoke(Request $request, Response $response): Response
+    {
+        $view = Twig::fromRequest($request);
+        $catalogsJson = $this->service->read('catalogs.json');
+        $catalogs = [];
+        if ($catalogsJson !== null) {
+            $catalogs = json_decode($catalogsJson, true) ?? [];
+        }
+
+        return $view->render($response, 'kataloge.twig', [
+            'kataloge' => $catalogs,
+        ]);
+    }
+}
