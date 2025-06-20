aa62d76bc120bfa020824439d26b64f9f925b156
calhelp <bastelix@web.de>
2025-06-21 01:28:48 +0200

    Guard cascade migration

--- migrations/20240622_add_cascade_to_questions.sql
+++ migrations/20240622_add_cascade_to_questions.sql
@@ -1,3 +1,11 @@
-ALTER TABLE questions DROP CONSTRAINT IF EXISTS questions_catalog_id_fkey;
-ALTER TABLE questions ADD CONSTRAINT questions_catalog_id_fkey
-    FOREIGN KEY (catalog_id) REFERENCES catalogs(id) ON DELETE CASCADE;
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1 FROM information_schema.columns
+        WHERE table_name = 'questions' AND column_name = 'catalog_id'
+    ) THEN
+        ALTER TABLE questions DROP CONSTRAINT IF EXISTS questions_catalog_id_fkey;
+        ALTER TABLE questions ADD CONSTRAINT questions_catalog_id_fkey
+            FOREIGN KEY (catalog_id) REFERENCES catalogs(id) ON DELETE CASCADE;
+    END IF;
+END$$;
