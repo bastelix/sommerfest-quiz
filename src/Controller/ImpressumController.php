ffdcca95938d5fdaafd8fbdeb327b457e9dcab9a
calhelp <bastelix@web.de>
2025-07-22 09:47:34 +0200

    feat: add admin page editor

--- src/Controller/ImpressumController.php
+++ src/Controller/ImpressumController.php
@@ -6,19 +6,19 @@ namespace App\Controller;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Views\Twig;
 
 /**
  * Displays the legal notice page.
  */
 class ImpressumController
 {
-    /**
-     * Render the Impressum page.
-     */
     public function __invoke(Request $request, Response $response): Response
     {
-        $view = Twig::fromRequest($request);
-        return $view->render($response, 'impressum.twig');
+        $path = dirname(__DIR__, 2) . '/content/impressum.html';
+        if (!is_file($path)) {
+            return $response->withStatus(404);
+        }
+        $response->getBody()->write((string) file_get_contents($path));
+        return $response->withHeader('Content-Type', 'text/html');
     }
 }
