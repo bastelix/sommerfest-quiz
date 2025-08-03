efd99ff3a63d3c6fff4572db7c11e7610f675c08
calhelp <bastelix@web.de>
2025-08-03 16:45:42 -0400

    feat: support profile variables in pages

---
+++ tests/Service/PageVariableServiceTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Service;
+
+use PHPUnit\Framework\TestCase;
+use App\Service\PageVariableService;
+
+class PageVariableServiceTest extends TestCase
+{
+    public function testApplyReplacesPlaceholders(): void
+    {
+        $html = '<p>[NAME], [STREET], [ZIP] [CITY], [EMAIL]</p>';
+        $result = PageVariableService::apply($html);
+        $this->assertStringNotContainsString('[NAME]', $result);
+        $this->assertStringNotContainsString('[STREET]', $result);
+        $this->assertStringNotContainsString('[ZIP]', $result);
+        $this->assertStringNotContainsString('[CITY]', $result);
+        $this->assertStringNotContainsString('[EMAIL]', $result);
+    }
+}
