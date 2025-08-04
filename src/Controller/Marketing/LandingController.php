61b5e5d600a286d3f6f4be4e55b58c41d8f77411
calhelp <bastelix@web.de>
2025-08-04 01:58:52 -0400

    Render marketing landing page within layout and remove image assets

--- src/Controller/Marketing/LandingController.php
+++ src/Controller/Marketing/LandingController.php
@@ -6,8 +6,8 @@ namespace App\Controller\Marketing;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Views\Twig;
 use Slim\Routing\RouteContext;
+use Slim\Views\Twig;
 
 /**
  * Displays the landing page for the marketing site.
