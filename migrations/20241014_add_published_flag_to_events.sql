0c657d3779130de3763e9a0d51f699bac556f151
calhelp <bastelix@web.de>
2025-08-15 22:19:54 +0200

    Make events.published migration idempotent

--- migrations/20241014_add_published_flag_to_events.sql
+++ migrations/20241014_add_published_flag_to_events.sql
@@ -1,3 +1,15 @@
-ALTER TABLE events ADD COLUMN IF NOT EXISTS published BOOLEAN;
-UPDATE events SET published = TRUE WHERE published IS NULL;
+DO $$
+BEGIN
+   IF NOT EXISTS (
+      SELECT 1
+      FROM information_schema.columns
+      WHERE table_name='events'
+        AND column_name='published'
+   ) THEN
+      ALTER TABLE events ADD COLUMN published BOOLEAN DEFAULT FALSE;
+      UPDATE events SET published = TRUE;
+   END IF;
+END $$;
+
 ALTER TABLE events ALTER COLUMN published SET DEFAULT FALSE;
+UPDATE events SET published = TRUE WHERE published IS NULL;
