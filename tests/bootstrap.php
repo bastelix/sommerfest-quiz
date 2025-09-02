47b7d772516d81d4c198f27eba4a853d71f17de5
calhelp <office@calhelp.de>
2025-09-02 14:07:17 +0200

    Extract NullRedirectManager stub

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,4 +3,5 @@
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/TestCase.php';
 require __DIR__ . '/Service/ArrayLogger.php';
+require __DIR__ . '/Service/NullRedirectManager.php';
 require __DIR__ . '/Uri.php';
