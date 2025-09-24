a14435095c65fc38b70a7c0a0b880dd831686f46
calhelp <office@calhelp.de>
2025-09-24 10:37:18 +0200

    Add contact block to calServer marketing page

--- src/Controller/Marketing/CalserverController.php
+++ src/Controller/Marketing/CalserverController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller\Marketing;
 
+use App\Service\MailService;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
@@ -16,6 +17,12 @@ class CalserverController
     public function __invoke(Request $request, Response $response): Response
     {
         $view = Twig::fromRequest($request);
-        return $view->render($response, 'marketing/calserver.twig');
+        $csrf = $_SESSION['csrf_token'] ?? bin2hex(random_bytes(16));
+        $_SESSION['csrf_token'] = $csrf;
+
+        return $view->render($response, 'marketing/calserver.twig', [
+            'mailConfigured' => MailService::isConfigured(),
+            'csrf_token' => $csrf,
+        ]);
     }
 }
