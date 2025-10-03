7cdd14dd572df72bbf7fafcc9e65bc51ff350861
calhelp <office@calhelp.de>
2025-10-03 13:49:08 +0200

    Handle calserver maintenance domain start page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -96,6 +96,11 @@ class HomeController
                     $ctrl = new \App\Controller\Marketing\CalserverController();
                     return $ctrl($request, $response);
                 }
+            } elseif ($home === 'calserver-maintenance') {
+                if ($catalogParam === '') {
+                    $ctrl = new \App\Controller\Marketing\MarketingPageController('calserver-maintenance');
+                    return $ctrl($request, $response);
+                }
             } elseif ($home === 'future-is-green') {
                 if ($catalogParam === '') {
                     $ctrl = new \App\Controller\Marketing\FutureIsGreenController();
